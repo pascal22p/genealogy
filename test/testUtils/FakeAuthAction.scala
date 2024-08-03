c21136b9c64b0ae45c5f737b262c53e8c19b0416
Pascal Parois <5671193+pascal22p@users.noreply.github.com>
2024-08-03 23:52:40 +0100

    tests queries (#23)
    * tests queries

--- test/testUtils/FakeAuthAction.scala
+++ test/testUtils/FakeAuthAction.scala
@@ -1,20 +1,27 @@
 package testUtils
 
+import scala.concurrent.ExecutionContext
+import scala.concurrent.Future
+
 import actions.AuthAction
-import models.{AuthenticatedRequest, Session, SessionData}
-import play.api.mvc.{AnyContent, BodyParser, Request, Result}
+import models.AuthenticatedRequest
+import models.Session
+import models.SessionData
+import play.api.mvc.AnyContent
+import play.api.mvc.BodyParser
+import play.api.mvc.Request
+import play.api.mvc.Result
 
-import scala.concurrent.{ExecutionContext, Future}
-
-class FakeAuthAction (localSession: Session) extends AuthAction {
+class FakeAuthAction(localSession: Session) extends AuthAction {
   def parser: BodyParser[AnyContent] = play.api.test.Helpers.stubBodyParser()
 
   def invokeBlock[A](request: Request[A], block: AuthenticatedRequest[A] => Future[Result]): Future[Result] = {
     val seePrivacy = request.headers.get("seePrivacy").exists(_.toBoolean)
-    val userData = request.headers.get("userData").forall(_.toBoolean)
+    val userData   = request.headers.get("userData").forall(_.toBoolean)
     val newSession = (userData, seePrivacy) match {
-      case (false, _) =>     Session("1", localSession.sessionData.copy(userData = None))
-      case (true, privacy) => Session("1", SessionData(1, localSession.sessionData.userData.map(_.copy(seePrivacy = privacy))))
+      case (false, _) => Session("1", localSession.sessionData.copy(userData = None))
+      case (true, privacy) =>
+        Session("1", SessionData(1, localSession.sessionData.userData.map(_.copy(seePrivacy = privacy))))
     }
     block(AuthenticatedRequest(request, newSession))
   }
