998eafe22417a96ab694c9335d35c04a096158e8
Pascal Parois <5671193+pascal22p@users.noreply.github.com>
2024-09-22 00:04:53 +0100

    refactor twirls components

--- app/models/HistoryElement.scala
+++ app/models/HistoryElement.scala
@@ -1,11 +1,10 @@
 package models
 
-import play.api.libs.json.OFormat
 import play.api.libs.json.Json
+import play.api.libs.json.OFormat
 
 final case class HistoryElement(personId: Int, name: String)
 
 object HistoryElement {
   implicit val format: OFormat[HistoryElement] = Json.format[HistoryElement]
 }
-
