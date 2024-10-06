165633066cce01cb4b445fb607b63725edf738cf
Pascal Parois <5671193+pascal22p@users.noreply.github.com>
2024-10-06 19:27:17 +0100

    added logout link (#61)
    * added logout link

---
+++ app/controllers/LogoutController.scala
@@ -0,0 +1,40 @@
+package controllers
+
+import javax.inject.Inject
+import javax.inject.Singleton
+
+import scala.concurrent.ExecutionContext
+import scala.concurrent.Future
+
+import actions.AuthAction
+import models.forms.UserDataForm
+import play.api.data.Form
+import play.api.i18n.I18nSupport
+import play.api.mvc.Action
+import play.api.mvc.AnyContent
+import play.api.mvc.BaseController
+import play.api.mvc.ControllerComponents
+import play.api.mvc.Result
+import play.api.mvc.Results.Redirect
+import queries.SessionSqlQueries
+import services.LoginService
+import views.html.Login
+import play.api.http.HeaderNames
+
+@Singleton
+class LogoutController @Inject() (
+    authAction: AuthAction,
+    sqlQueries: SessionSqlQueries,
+    val controllerComponents: ControllerComponents
+)(
+    implicit ec: ExecutionContext
+) extends BaseController
+    with I18nSupport {
+
+  def onLoad: Action[AnyContent] = authAction.async { implicit authenticatedRequest =>
+    sqlQueries.removeSessionData(authenticatedRequest.localSession)
+    val returnUrl = authenticatedRequest.request.headers.get(HeaderNames.REFERER).getOrElse(routes.HomeController.onload().url)
+    Future.successful(Redirect(returnUrl))
+  }
+
+}
--- app/models/forms/UserDataForm.scala
+++ app/models/forms/UserDataForm.scala
@@ -4,15 +4,16 @@ import play.api.data.Form
 import play.api.data.Forms.mapping
 import play.api.data.Forms.nonEmptyText
 
-final case class UserDataForm(username: String, password: String)
+final case class UserDataForm(username: String, password: String, returnUrl: String)
 
 object UserDataForm {
-  def unapply(u: UserDataForm): Option[(String, String)] = Some((u.username, u.password))
+  def unapply(u: UserDataForm): Option[(String, String, String)] = Some((u.username, u.password, u.returnUrl))
 
   val userForm: Form[UserDataForm] = Form(
     mapping(
-      "username" -> nonEmptyText,
-      "password" -> nonEmptyText
+      "username"  -> nonEmptyText,
+      "password"  -> nonEmptyText,
+      "returnUrl" -> nonEmptyText
     )(UserDataForm.apply)(UserDataForm.unapply)
   )
 }
