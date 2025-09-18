60e6a7e33cac06377669859478b53839e9cafa75
Pascal Parois <5671193+pascal22p@users.noreply.github.com>
2025-09-18 15:27:21 +0100

    tweak logging

--- app/filters/RequestAttrFilter.scala
+++ app/filters/RequestAttrFilter.scala
@@ -28,7 +28,7 @@ class RequestAttrFilter @Inject() (
       .addAttr(Attrs.RequestId, requestId)
       .addAttr(Attrs.SessionId, sessionId)
 
-    logger.debug(s"RequestIdAttrFilter assigned requestId=$requestId to ${request.method} ${request.uri}")
+    logger.info(s"RequestAttrFilter assigned sessionId=`$sessionId` requestId=`$requestId` to ${request.method} ${request.uri}")
 
     // Also put it on the response headers (optional, useful for clients/debugging)
     next(enrichedRequest).map { req =>
