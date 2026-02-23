dad032671de7d50cf34faa3be799bc37ae925076
Pascal Parois <5671193+pascal22p@users.noreply.github.com>
2026-02-23 14:12:08 +0000

    test both compile checks

--- test/models/journeyCache/UserAnswersTypeSafetySpec.scala
+++ test/models/journeyCache/UserAnswersTypeSafetySpec.scala
@@ -8,34 +8,34 @@ import testUtils.BaseSpec
 
 class UserAnswersTypeSafetySpec extends BaseSpec {
 
-  "upsert rejects wrong type" ignore {
+  "upsert rejects wrong type" in {
     // this does not work in a github action runner environment
 
     val errors: List[Error] = typeCheckErrors("""
-        import models.forms.{GedcomListForm, NewDatabaseForm}
-        import models.journeyCache.UserAnswersKey.GedcomPath
+        import models.forms.{GedcomListForm, CreateNewDatabaseForm}
+        import models.journeyCache.UserAnswersKey.ChooseGedcomFileQuestion
         import repositories.MariadbJourneyCacheRepository
         import models.journeyCache.{UserAnswersItem, UserAnswersKey}
 
         val repo: MariadbJourneyCacheRepository = ???
-        repo.upsert(GedcomPath, NewDatabaseForm("", ""))(???, ???)
+        repo.upsert(ChooseGedcomFileQuestion, CreateNewDatabaseForm("", ""))(???, ???)
       """)
 
     withClue(errors.mkString("\n")) {
-      assert(errors.exists(_.message.contains("Found:    models.forms.NewDatabaseForm")))
-      assert(errors.exists(_.message.contains("Required: models.forms.GedcomListForm")))
+      assert(errors.exists(_.message.contains("Found:    models.forms.CreateNewDatabaseForm")))
+      assert(errors.exists(_.message.contains("Required: models.forms.GedcomPathInputTextForm")))
     }
   }
 
   "upsert rejects wrong type bis" in {
     """
-          import models.forms.{GedcomListForm, CreateNewDatabaseForm}
-          import models.journeyCache.UserAnswersKey.ChooseGedcomFileQuestion
-          import repositories.MariadbJourneyCacheRepository
-          import models.journeyCache.{UserAnswersItem, UserAnswersKey}
+        import models.forms.{GedcomListForm, CreateNewDatabaseForm}
+        import models.journeyCache.UserAnswersKey.ChooseGedcomFileQuestion
+        import repositories.MariadbJourneyCacheRepository
+        import models.journeyCache.{UserAnswersItem, UserAnswersKey}
 
-          val repo: MariadbJourneyCacheRepository = ???
-          repo.upsert(ChooseGedcomFileQuestion, CreateNewDatabaseForm("", ""))(???, ???)
-        """ shouldNot compile
+        val repo: MariadbJourneyCacheRepository = ???
+        repo.upsert(ChooseGedcomFileQuestion, CreateNewDatabaseForm("", ""))(???, ???)
+      """ shouldNot typeCheck
   }
 }
