efeca5a2169b4580b9300f3837c4ad6a929cd600
Pascal Parois <5671193+pascal22p@users.noreply.github.com>
2026-02-28 20:30:53 +0000

    replacing bcrypt with a more mainstream library

--- project/LibDependencies.scala
+++ project/LibDependencies.scala
@@ -8,16 +8,16 @@ object LibDependencies {
     "org.mariadb.jdbc"        %  "mariadb-java-client" % "3.5.7",
     "org.playframework.anorm" %% "anorm"               % "2.9.1",
     "org.typelevel"           %% "cats-core"           % "2.13.0",
-    "org.mindrot"             %  "jbcrypt"             % "0.4",
+    "com.password4j"          %  "password4j"          % "1.8.4",
     "uk.gov.hmrc"             %% "play-frontend-hmrc-play-30" % "12.32.0",
-    "org.apache.xmlgraphics"  % "fop"                  % "2.11"
+    "org.apache.xmlgraphics"  %  "fop"                 % "2.11"
   )
 
   val libraryDependenciesTest: Seq[ModuleID] = Seq(
-    "org.scalatestplus.play"  %% "scalatestplus-play" % "7.0.2" % Test,
-    "org.scalatestplus"       %%  "mockito-5-10"      % "3.2.18.0" % Test,
-    "org.jsoup"                % "jsoup"              % "1.22.1"
-  )
+    "org.scalatestplus.play"  %% "scalatestplus-play" % "7.0.2",
+    "org.scalatestplus"       %% "mockito-5-10"       % "3.2.18.0",
+    "org.jsoup"               %  "jsoup"              % "1.22.1"
+  ).map(_ % Test)
 
   val all: Seq[ModuleID]  = libraryDependenciesCompile ++ libraryDependenciesTest
 }
