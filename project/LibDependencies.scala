c3c51547ec60ff543098711748ba38b67f51ab64
Pascal Parois <5671193+pascal22p@users.noreply.github.com>
2026-03-22 19:55:38 +0000

    added logback

--- project/LibDependencies.scala
+++ project/LibDependencies.scala
@@ -10,7 +10,8 @@ object LibDependencies {
     "org.typelevel"           %% "cats-core"           % "2.13.0",
     "com.password4j"          %  "password4j"          % "1.8.4",
     "uk.gov.hmrc"             %% "play-frontend-hmrc-play-30" % "13.0.0",
-    "org.apache.xmlgraphics"  %  "fop"                 % "2.11"
+    "org.apache.xmlgraphics"  %  "fop"                 % "2.11",
+    "net.logstash.logback" % "logstash-logback-encoder" % "9.0"
   )
 
   val libraryDependenciesTest: Seq[ModuleID] = Seq(
