914f507d212f1f4b74d009ebff5f1749eb47de01
Pascal Parois <5671193+pascal22p@users.noreply.github.com>
2026-03-23 11:36:57 +0000

    improve logging of the request id (#331)

--- app/models/DatabaseExecutionContext.scala
+++ app/models/DatabaseExecutionContext.scala
@@ -1,10 +1,26 @@
 package models
 
-import javax.inject._
+import javax.inject.*
 
 import org.apache.pekko.actor.ActorSystem
+import org.slf4j.MDC
 import play.api.libs.concurrent.CustomExecutionContext
 
 @Singleton
 class DatabaseExecutionContext @Inject() (system: ActorSystem)
-    extends CustomExecutionContext(system, "database.dispatcher")
+    extends CustomExecutionContext(system, "database.dispatcher") {
+
+  override def execute(runnable: Runnable): Unit = {
+    val mdcContext = MDC.getCopyOfContextMap
+    super.execute { () =>
+      val previous = MDC.getCopyOfContextMap
+      if (mdcContext != null) MDC.setContextMap(mdcContext)
+      else MDC.clear()
+      try runnable.run()
+      finally {
+        if (previous != null) MDC.setContextMap(previous)
+        else MDC.clear()
+      }
+    }
+  }
+}
--- app/models/LoggingWithRequest.scala
+++ app/models/LoggingWithRequest.scala
@@ -1,6 +1,7 @@
 package models
 
 import models.Attrs
+import org.slf4j.MDC
 import org.slf4j.MarkerFactory
 import play.api.mvc.RequestHeader
 import play.api.Logging
@@ -8,11 +9,26 @@ import play.api.MarkerContext
 
 trait LoggingWithRequest extends Logging {
 
+  @SuppressWarnings(Array("org.wartremover.warts.RedundantConversions"))
   implicit def requestHeaderToMarkerContext(implicit request: RequestHeader): MarkerContext = {
     val requestId = request.attrs(Attrs.RequestId)
     val sessionId = request.attrs(Attrs.SessionId)
-    val marker    = MarkerFactory.getDetachedMarker(s"requestId=$requestId, sessionId=$sessionId")
+
+    // Set on MDC so all loggers pick it up
+    MDC.put("request_id", requestId.toString)
+    MDC.put("session_id", sessionId.toString)
+
+    val marker = MarkerFactory.getDetachedMarker(s"requestId=$requestId, sessionId=$sessionId")
     MarkerContext(marker)
+  }
+
+  def withRequestLogging[A](block: => A)(implicit request: RequestHeader): A = {
+    requestHeaderToMarkerContext
+    try block
+    finally {
+      MDC.remove("request_id")
+      MDC.remove("session_id")
+    }
   }
 
 }
