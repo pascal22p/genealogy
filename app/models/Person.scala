14349ab4105a6d18e1c3f7c0eada61ee25e4b5bd
Pascal Parois <5671193+pascal22p@users.noreply.github.com>
2026-06-19 20:40:00 +0100

    fixed bug in descendants (#372)

--- app/models/Person.scala
+++ app/models/Person.scala
@@ -19,6 +19,18 @@ final case class Person(
     }
   }
 
+  def findPartner(familyId: Int): Option[Person] = {
+    families.find(_.id == familyId).flatMap { family =>
+      if (family.parent1.exists(_.details.id != details.id)) {
+        family.parent1
+      } else if (family.parent2.exists(_.details.id != details.id)) {
+        family.parent2
+      } else {
+        None
+      }
+    }
+  }
+
   def name: String = {
     s"${details.firstname} ${details.surname}"
   }
